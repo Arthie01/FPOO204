dc6ce7a1ee2a197dbf2e11d6c91e7ea10d454e48
Arthie01 <123045452@upq.edu.mx>
2025-01-22 14:42:33 -0600

    Estrcutura de un archivo java y manejo de cadenas

---
+++ Clase 1/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    //Importaciones
+
+    //CLse principal
+    public static void main(String[] args) {
+
+        System.out.println("Mi nombre es Artemio");
+
+        String cadenas = "Artemio" + " " + "Hurtado" + " " + "Hernández";
+        
+        System.out.println(cadenas);
+
+        System.out.println(cadenas.length());
+
+        System.out.println(cadenas.substring(2,5));
+        System.out.println(cadenas.substring(0));
+        System.out.println(cadenas.substring(0,5));
+
+    }
+}
--- Clase 1/src/Main1.java
+++
@@ -1,8 +0,0 @@
-public class Main1 {
-    public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
-
-        System.out.println("Mi nombre es Artemio");
-    }
-}
