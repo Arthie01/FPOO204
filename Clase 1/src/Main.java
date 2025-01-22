89f2cf189707000c82361fe8b08360cde29776b2
Arthie01 <123045452@upq.edu.mx>
2025-01-22 15:35:03 -0600

    FInalizar práctica No2 Sintaxis

--- Clase 1/src/Main.java
+++ Clase 1/src/Main.java
@@ -1,3 +1,6 @@
+//import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     //Importaciones
@@ -5,17 +8,77 @@ public class Main {
     //CLse principal
     public static void main(String[] args) {
 
-        System.out.println("Mi nombre es Artemio");
 
-        String cadenas = "Artemio" + " " + "Hurtado" + " " + "Hernández";
+        //2.- Cadenas con java
+
+
+        /* 
+        //3.-Variables 
+
+        int x = 5, x2;
+        double y = 1.25, y2;
+        String z = "204", z1;
+
+
+        x2 = Integer.parseInt(z);
+        z1 = String.valueOf(y);
+
+
+        // Conversion de int a double
+        double asd = Double.valueOf(x);
+
+        //Conversion implicita
+
+        int num = 12;
+        double num0 = num;
+        System.out.println("Conversión implicita: " + num0);
+
+        System.out.println(x2);
+        System.out.println(z1);
+        System.out.println(asd);
+
+        Random  rdn = new Random();
+
+        int numAleatorio = rdn.nextInt(100);
+        double numAleatorio2 = rdn.nextDouble();
+
+        System.out.println("Aleatorio entero: " + numAleatorio);*/
+
+        //4.- Solicitud de datos
+
+        /*Scanner scn = new Scanner(System.in);
+
+        System.out.println("Introduce cualquier dato cualquiera");
+        String datos = scn.nextLine();
+
+        System.out.println("Introduce cualquier dato entero");
+        double datoEntero = scn.nextInt();
+
+        System.out.println("Introduce cualquier dato decimal");
+        double datoDecimal = scn.nextDouble();
         
-        System.out.println(cadenas);
 
-        System.out.println(cadenas.length());
 
-        System.out.println(cadenas.substring(2,5));
-        System.out.println(cadenas.substring(0));
-        System.out.println(cadenas.substring(0,5));
+        System.out.print("Cualquier dato: " + datos);
+        scn.nextLine();
+        System.out.print("Dato entero: " +  datoEntero);
+        scn.nextLine();
+        System.out.print("Dato decimal: " + datoDecimal);
+        scn.nextLine();*/
+
+        //5.- Boolean, peradores logicos y de comparación
+
+        System.out.println(10>9);
+        System.out.println(10==9);
+        System.out.println(10<9);
+        System.out.println(10>=9);
+        System.out.println(10<=9);
+        System.out.println(10!=9);
+
+        int x = 3:
+        System.out.println(x < 5 && x>10);
+        System.out.println(x < 5 || x>10);
+        System.out.println(!(x < 5  && x>10));
 
     }
 }
